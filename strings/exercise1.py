f44c4fbc47767e0a5f8bc715a6fdadb500eb7861
Learner890 <nishantv520@gmail .com>
2024-03-10 22:08:08 +0530

    List and string questions

---
+++ strings/exercise1.py
@@ -0,0 +1,7 @@
+street='Street number 2'
+city='Mumbai'
+country='India'
+Address=street+'\n' +city+'\n' +country
+print(Address)
+Address2=f"{street} \n{city} \n{country}"
+print(Address2)
---
+++ strings/exercise2.py
@@ -0,0 +1,8 @@
+variable="Earth revolves around the sun"
+print(variable[6:14])
+print(variable[-3:])
+
+s='maine 200 banana khaye'
+s=s.replace('banana','samosa').replace('200','20')
+print(s)
+print(id(s))
